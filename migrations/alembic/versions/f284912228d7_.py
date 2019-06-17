acdf95d1566c7beab0242f6b16887df6a3208b3a
Cassandra Heide <cassandraheide@gmail.com>
2019-06-17 15:05:50 -0400

    cleanup

---
+++ migrations/alembic/versions/f284912228d7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f284912228d7
+Revises: 303d03a880ab, b1695cff6446
+Create Date: 2019-06-17 14:54:33.892317
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f284912228d7'
+down_revision = ('303d03a880ab', 'b1695cff6446')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
