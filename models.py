b2ec3086f23bb019623e5dd170e3fd9de7b0c56e
Cassandra Heide <cassandraheide@gmail.com>
2019-06-18 10:45:43 -0400

    simplify data model

--- models.py
+++ models.py
@@ -21,33 +21,23 @@ class Station(db.Model):
 		
 class Historical(db.Model):
 	__tablename__ = 'historicals'
-	year = db.Column(db.Integer)
-	month = db.Column(db.Integer)
-	day = db.Column(db.Integer)
-	time = db.Column(db.Integer)
-	datetime = db.Column(db.DateTime, primary_key=True)
+	id = db.Column(db.DateTime, primary_key=True)
 	drybulb = db.Column(db.Integer)
 	dewpoint = db.Column(db.Integer)
 	relative_humidity = db.Column(db.Integer)
-	wind_dir_10s_deg = db.Column(db.Integer)
-	wind_spd_kmh = db.Column(db.Integer)
-	visibility_km = db.Column(db.Integer)
-	stn_press_kpa = db.Column(db.Integer)
-	hmdx = db.Column(db.Integer)
-	wind_chill = db.Column(db.Integer)
-	weather = db.Column(db.Integer)
 	
 	def __init__(self, drybulb, dewpoint):
 		self.drybulb = drybulb
 		self.dewpoint = dewpoint
-		self.timestamp = datetime
 
+	
 	def __repr__(self):
-		return '<timestamp {}>'.format(self.datetime)
+		return '<id {}>'.format(self.id)
+
 
 class Forecast(db.Model):
 	__tablename__ = 'forecasts'
-	datetime = db.Column(db.DateTime, primary_key=True)
+	id = db.Column(db.DateTime, primary_key=True)
 	drybulb = db.Column(db.Integer)
 	dewpoint = db.Column(db.Integer)
 	relative_humidity = db.Column(db.Integer)
