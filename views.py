57ca59706303ff8e6b14a83fbd4757f4161f0ba6
Cassandra Heide <cassandraheide@gmail.com>
2019-06-21 16:50:04 -0400

    24h function

--- views.py
+++ views.py
@@ -25,98 +25,63 @@ def index():
 def create_plot():
     N = 40
     now = datetime.datetime.now()
-    x00 = [x.id for x in Historical.query.filter(Historical.id >= now - datetime.timedelta(days=7)).order_by(Historical.id)]
-    y00 = [x.drybulb for x in Historical.query.filter(Historical.id >= now - datetime.timedelta(days=7)).order_by(Historical.id)]
 
     # actuals
     x0 = [x.id for x in Current.query.filter(Current.id >= now - datetime.timedelta(days=7)).order_by(Current.id)]
     y0 = [x.drybulb for x in Current.query.filter(Current.id >= now - datetime.timedelta(days=7)).order_by(Current.id)]
 
-    # latest forecast (retrieved within the last hour)   
+    # latest
     x1 = [x.id for x in Forecast.query.filter(Forecast.retrieval_time >= now - datetime.timedelta(hours=1))
           .order_by(Forecast.id)]
 
     y1 = [x.drybulb for x in Forecast.query.filter(Forecast.retrieval_time >= now - datetime.timedelta(hours=1))
           .order_by(Forecast.id)]
 
-    # 6h   
-    x4 = [x.id for x in Forecast.query.filter(Forecast.retrieval_time >= now - datetime.timedelta(hours=6),
-    	Forecast.retrieval_time < now - datetime.timedelta(minutes=355)).order_by(Forecast.id)]
-
-    y4 = [x.drybulb for x in Forecast.query.filter(Forecast.retrieval_time >= now - datetime.timedelta(hours=6),
-    	Forecast.retrieval_time < now - datetime.timedelta(minutes=355)).order_by(Forecast.id)]
-        
-    # retrieved on roughly this hour, 1d ago
-    # datetime greater than now minus 1d; less than five minutes after now minus 1d   
-    x2 = [x.id for x in Forecast.query.filter(Forecast.retrieval_time > now - datetime.timedelta(days=1),
-        Forecast.retrieval_time < now - datetime.timedelta(minutes=1435)).order_by(Forecast.id)]
-
-    y2 = [x.drybulb for x in Forecast.query.filter(Forecast.retrieval_time > now - datetime.timedelta(days=1),
-        Forecast.retrieval_time < now - datetime.timedelta(minutes=1435)).order_by(Forecast.id)]
-
-    # retrieved on roughly this hour, 2d ago
-    # datetime greater than now minus 2d; less than five minutes after now minus 2d   
-    x3 = [x.id for x in Forecast.query.filter(Forecast.retrieval_time > now - datetime.timedelta(days=2),
-        Forecast.retrieval_time < now - datetime.timedelta(minutes=2875)).order_by(Forecast.id)]
-
-    y3 = [x.drybulb for x in Forecast.query.filter(Forecast.retrieval_time > now - datetime.timedelta(days=2),
-        Forecast.retrieval_time < now - datetime.timedelta(minutes=2875)).order_by(Forecast.id)]
-
-    historicals = go.Scatter(
-            x=x00,
-            y=y00,
-            name='historicals',
-            line=dict(
-                color = ('rgb(192,192,192)'),
-                width = 2,)
-            )
+    w=4
 
     actuals = go.Scatter(
-            x=x0,
-            y=y0,
-            name='actuals',
-            mode='lines',
-            line=dict(
-                color = ('rgb(255,0,127)'),
-                width = 3,)
-            )
-    
-    forecasts_latest = go.Scatter(
-            x=x1,
-            y=y1,
-            name='forecasts_latest',
-            line=dict(
-                color = ('rgb(0,51,102)'),
-                width = 2,)
-            )
+        x=x0,
+        y=y0,
+        name='actual temperature',
+        mode='lines',
+        line=dict(
+            color = ('rgb(255,0,127)'),
+            width = w)
+        )   
 
-    forecasts_1d = go.Scatter(
-            x=x2,
-            y=y2,
-            name='forecasts_1d',
-            line=dict(
-                color = ('rgb(0,76,153)'),
-                width = 2,)
-            )
+    latest = go.Scatter(
+        x=x1,
+        y=y1,
+        name='latest_forecast',
+        mode='lines',
+        line=dict(
+            color = ('rgb(153,0,153)'),
+            width = w)
+        )
 
-    forecasts_2d = go.Scatter(
-            x=x3,
-            y=y3,
-            name='forecasts_2d',
-            line=dict(
-                color = ('rgb(0,102,204)'),
-                width = 2,)
-            )
+    data = [actuals, latest]
 
-    forecasts_6h = go.Scatter(
-            x=x4,
-            y=y4,
-            name='forecasts_6h',
-            line=dict(
-                color = ('rgb(0,128,255)'),
-                width = 2,)
-            )
+    # hourly for past n hours 
+    for n in range(1,24):
+        x = [x.id for x in Forecast.query.filter(Forecast.retrieval_time >= now - datetime.timedelta(hours=n),
+            Forecast.retrieval_time < now - datetime.timedelta(minutes=355)).order_by(Forecast.id)]
 
+        y = [x.drybulb for x in Forecast.query.filter(Forecast.retrieval_time >= now - datetime.timedelta(hours=n),
+            Forecast.retrieval_time < now - datetime.timedelta(minutes=355)).order_by(Forecast.id)]   
+
+
+        forecasts = go.Scatter(
+                x=x,
+                y=y,
+                name='forecasts_{}h'.format(n),
+                line=dict(
+                    color = ('rgb(0,76,153)'),
+                    width = w*(1/n)
+                    )
+                )
+        data.append(forecasts)
+
+# Layout
     layout = go.Layout(
     title=go.layout.Title(
         text='Weather Forecast Tracker',
@@ -148,8 +113,6 @@ def create_plot():
 	)
 
     
-
-    data = [historicals, actuals, forecasts_latest, forecasts_6h, forecasts_1d, forecasts_2d]
     fig = go.Figure(data=data, layout=layout)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
