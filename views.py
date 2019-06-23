b4aa7fd8e46dd91bfaf3ff34dbe1e051d2d20f69
Cassandra Heide <cassandraheide@gmail.com>
2019-06-22 23:03:39 -0400

    add second chart

--- views.py
+++ views.py
@@ -11,6 +11,7 @@ import datetime
 import plotly
 from plotly import graph_objs as go
 from plotly import plotly as py 
+from plotly import tools
 import matplotlib as mpl
 
 import pandas as pd
@@ -22,7 +23,8 @@ import json
 def index():   
 
     plot = create_plot()
-    return render_template("index.html", plot=plot)    
+    plot2 = create_plot2()
+    return render_template("index.html", plot=plot, plot2=plot2)    
     
 def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
     c1=np.array(mpl.colors.to_rgb(c1))
@@ -32,7 +34,6 @@ def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0)
 def create_plot():
     
     # Variables
-    N = 40
     w=4
     h=48
     now = datetime.datetime.now().replace(microsecond=0,second=0,minute=0)
@@ -124,3 +125,45 @@ def create_plot():
     fig = go.Figure(data=data, layout=layout)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
+
+def create_plot2():
+    data=[]
+    now = datetime.datetime.now().replace(microsecond=0,second=0,minute=0)
+
+    # Layout
+    layout = go.Layout(
+    title=go.layout.Title(
+        text='Weather Forecast Tracker 2',
+        font=dict(
+                family='Helvetica, monospace',
+                size=20
+                )
+    ),
+
+    xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(
+            text='Week of {}'.format((now - datetime.timedelta(days=now.weekday())).date()),
+            font=dict(
+                family='Helvetica, monospace',
+                size=18
+            )
+        )
+    ),
+
+    yaxis=go.layout.YAxis(
+        title=go.layout.yaxis.Title(
+            text='Temperature \u2103',
+            font=dict(
+                family='Helvetica, monospace',
+                size=18
+	            )
+	        )
+	    )
+	)
+
+    fig = go.Figure(data=data, layout=layout)
+    graphJSON2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON2
+
+
+
